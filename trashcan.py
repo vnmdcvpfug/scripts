53360b73416c8302de4503c74deb27cd331aa723
vnmdcvpfug <vnmdcvpfug@gmail.com>
2024-12-02 19:47:56 +0100

    initial commit

---
+++ trashcan.py
@@ -0,0 +1,18 @@
+import sys
+from pathlib import Path
+from send2trash import send2trash
+
+target = sys.argv[1]
+
+def dialogue():
+    prompt = input("Move '" + str(target) + "' to trash? y/N ")
+    if prompt in ['', 'y', 'yes', 'Y', 'Yes', 'YES']:
+        try:
+            send2trash(target)
+            print("'" + target + "' was moved to trash.")
+        except OSError:
+            print("No such file or directory.")
+    else:
+        pass
+
+dialogue()
